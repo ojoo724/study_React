5b7c45916d27f46df0f67d7046cc54b72172d1b6
ojoo724 <jae03025@naver.com>
2023-11-02 11:40:19 +0900

    11.02 - react start!

---
+++ dice_game/src/index.js
@@ -0,0 +1,32 @@
+import ReactDOM from 'react-dom/client';
+
+const WINS = {
+  rock: 'scissor',
+  scissor: 'paper',
+  paper: 'rock',
+};
+
+function getResult(left, right) {
+  if (WINS[left] === right) return '승리';
+  else if (left === WINS[right]) return '패배';
+  return '무승부';
+}
+
+function handleClick() {
+  console.log('가위바위보!');
+}
+
+const me = 'rock';
+const other = 'scissor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+ <>
+  <h1 id="title">가위바위보</h1>
+  <h2>{getResult(me, other)}</h2>
+  <button className="hand" onClick={handleClick}>가위</button>
+  <button className="hand" onClick={handleClick}>바위</button>
+  <button className="hand" onClick={handleClick}>보</button>
+ </>
+);
+
